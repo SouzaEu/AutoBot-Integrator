0e0fc81b5a326d3dbaed1a50e55a6d0a7d5124cb
SouzaEu <vinnysouzav@gmail.com>
2025-04-03 19:10:33 -0300

    adicionando projeto base

---
+++ src/main/java/com/autobot/integrator/AutobotIntegratorApplication.java
@@ -0,0 +1,13 @@
+package com.autobot.integrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobotIntegratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutobotIntegratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autobot/integrator/AutobotIntegratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autobot.integrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutobotIntegratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
